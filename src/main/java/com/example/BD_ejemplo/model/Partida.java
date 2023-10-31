80ef1007c22aac64f4fff0fe94e4c85dddb70c86
JulianBlanco98 <jblancoho@alumnos.unex.es>
2023-10-31 17:57:17 +0100

    test_usuarios_relleno

--- src/main/java/com/example/BD_ejemplo/model/Partida.java
+++ src/main/java/com/example/BD_ejemplo/model/Partida.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 
-//@Entity
+@Entity
 public class Partida {
 
 	@Id
--- src/test/java/com/example/BD_ejemplo/BdEjemploApplicationTests.java
+++ src/test/java/com/example/BD_ejemplo/BdEjemploApplicationTests.java
@@ -26,7 +26,19 @@ class BdEjemploApplicationTests {
 		Scanner in = new Scanner(System.in);
 
 		System.out.println("----------------EMPIEZAN LOS TEST----------------");
-
+		
+		
+		
+		//3 usuarios nuevos
+		Usuario usuario1 = new Usuario();
+		Usuario usuario2 = new Usuario();
+		Usuario usuario3 = new Usuario();
+		
+		//Rellenamos sus datos
+		usuario1.setNombre("Julian");usuario2.setNombre("Ignacio");usuario3.setNombre("Nicolas");
+		usuario1.setEdad(24);usuario2.setEdad(21);usuario3.setEdad(22);
+		usuario1.setContra("contra1");usuario2.setContra("contra2");usuario3.setContra("contra3");
+		usuario1.setDinero(1000L);usuario2.setDinero(1000L);usuario3.setDinero(1000L);
 		
 
 	}
