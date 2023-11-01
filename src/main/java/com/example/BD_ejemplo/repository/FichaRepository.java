78f13f15e4970601df80f697b5e8f965388b7852
ignn4 <igguerrer@alumnos.unex.es>
2023-11-01 16:55:21 +0100

    selecFIchas

--- src/main/java/com/example/BD_ejemplo/repository/FichaRepository.java
+++ src/main/java/com/example/BD_ejemplo/repository/FichaRepository.java
@@ -8,11 +8,14 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.BD_ejemplo.model.Ficha;
+import com.example.BD_ejemplo.model.Partida;
 
 
 
 @Repository
 public interface FichaRepository extends CrudRepository<Ficha, Long> {
 	
-
+	@Query("SELECT f FROM Ficha f WHERE f.tirada.idTirada = ?1")
+	List<Partida> findFichasByTiradaId(Long tiradaId);
+	
 }
