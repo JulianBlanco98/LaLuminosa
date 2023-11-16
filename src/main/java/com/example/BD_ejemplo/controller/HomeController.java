e6fea75ca1361a3df98a44abdbffc89b91645c5c
ignn4 <igguerrer@alumnos.unex.es>
2023-11-16 18:49:52 +0100

    Update HomeController.java

--- src/main/java/com/example/BD_ejemplo/controller/HomeController.java
+++ src/main/java/com/example/BD_ejemplo/controller/HomeController.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 	
-//	@GetMapping("/holaTH")
-//	public String holaPage(Model model) {
-//		String texto ="Hola mundo en ejecución";
-//		model.addAttribute("Bienvenida", texto);
-//		
-//		return "holaTH";
-//	}
+	@GetMapping("/")
+	public String holaPage(Model model) {
+		String texto ="Hola mundo en ejecución";
+		model.addAttribute("Bienvenida", texto);
+		
+		return "index";
+	}
 
 }
