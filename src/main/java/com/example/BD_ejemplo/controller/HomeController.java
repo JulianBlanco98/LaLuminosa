b26192d98f9ae20e9bbd5f8f87667ac28f6d2f5d
JulianBlanco98 <jblancoho@alumnos.unex.es>
2023-11-16 18:25:59 +0100

    Update HomeController.java

--- src/main/java/com/example/BD_ejemplo/controller/HomeController.java
+++ src/main/java/com/example/BD_ejemplo/controller/HomeController.java
@@ -1,5 +1,18 @@
 package com.example.BD_ejemplo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+	
+	@GetMapping("/holaTH")
+	public String holaPage(Model model) {
+		String texto ="Hola mundo en ejecución";
+		model.addAttribute("Bienvenida", texto);
+		
+		return "holaTH";
+	}
 
 }
