3016b75a2786934bb82b5b9b70df964fba26e322
JulianBlanco98 <jblancoho@alumnos.unex.es>
2023-12-06 22:45:45 +0100

    Update principal.js

--- src/main/resources/templates/JS/principal.js
+++ src/main/resources/templates/JS/principal.js
@@ -13,41 +13,41 @@ function initWheel() {
     row = "";
 
   row += "<div class='row'>";
-  row += "  <div class='card red'>1<\/div>";
   row += "  <div class='card black'>32<\/div>";
+  row += "  <div class='card red'>1<\/div>";
   row += "  <div class='card black'>23<\/div>";
   row += "  <div class='card red'>14<\/div>";
-  row += "  <div class='card red'>2<\/div>";
-  row += "  <div class='card red'>26<\/div>";
-  row += "  <div class='card red'>13<\/div>";
   row += "  <div class='card black'>21<\/div>";
+  row += "  <div class='card red'>2<\/div>";
   row += "  <div class='card black'>3<\/div>";
+  row += "  <div class='card red'>26<\/div>";
   row += "  <div class='card black'>20<\/div>";
-  row += "  <div class='card red'>18<\/div>";
-  row += "  <div class='card red'>12<\/div>";
-  row += "  <div class='card red'>29<\/div>";
+  row += "  <div class='card red'>13<\/div>";
   row += "  <div class='card black'>4<\/div>";
+  row += "  <div class='card red'>18<\/div>";
   row += "  <div class='card black'>34<\/div>";
+  row += "  <div class='card red'>12<\/div>";
   row += "  <div class='card black'>27<\/div>";
+  row += "  <div class='card red'>29<\/div>";
   row += "  <div class='card black'>16<\/div>";
   row += "  <div class='card red'>36<\/div>";
   row += "  <div class='card green'>0<\/div>";
   row += "  <div class='card black'>11<\/div>";
-  row += "  <div class='card black'>22<\/div>";
   row += "  <div class='card red'>5<\/div>";
+  row += "  <div class='card black'>22<\/div>";
   row += "  <div class='card red'>30<\/div>";
   row += "  <div class='card black'>10<\/div>";
   row += "  <div class='card red'>6<\/div>";
-  row += "  <div class='card red'>24<\/div>";
-  row += "  <div class='card red'>17<\/div>";
-  row += "  <div class='card red'>31<\/div>";
   row += "  <div class='card black'>9<\/div>";
+  row += "  <div class='card red'>24<\/div>";
   row += "  <div class='card black'>28<\/div>";
-  row += "  <div class='card red'>7<\/div>";
+  row += "  <div class='card red'>17<\/div>";
   row += "  <div class='card black'>35<\/div>";
+  row += "  <div class='card red'>31<\/div>";
   row += "  <div class='card black'>8<\/div>";
-  row += "  <div class='card red'>19<\/div>";
+  row += "  <div class='card red'>7<\/div>";
   row += "  <div class='card black'>15<\/div>";
+  row += "  <div class='card red'>19<\/div>";
   row += "  <div class='card black'>33<\/div>";
   row += "  <div class='card red'>25<\/div>";
   
@@ -60,7 +60,7 @@ function initWheel() {
 
 function spinWheel(roll) {
   var $wheel = $('.roulette-wrapper .wheel'),
-    order = [0, 11, 22, 5, 30, 10, 6, 24, 17, 31, 9, 28, 7, 35, 8, 19, 15, 33, 25, 1, 32, 23, 14, 2, 26, 13, 21, 3, 20, 18, 12, 29, 4, 34, 27, 16, 36],
+    order = [0, 11, 5, 22, 30, 10, 6, 9, 24, 28, 17, 35, 31, 8, 7, 15, 19, 33, 25, 32, 1, 23, 14, 21, 2, 3, 26, 20, 13, 4, 18, 34, 12, 27, 29, 16, 36],
     position = order.indexOf(roll);
   console.log("Array: " + order);
   console.log("Posicion: "+position);
@@ -76,13 +76,13 @@ function spinWheel(roll) {
   landingPosition = landingPosition + randomize;
 
   var object = {
-    x: Math.floor(Math.random() * 50) / 100,
+    x: Math.floor(Math.random() * 70) / 100,
     y: Math.floor(Math.random() * 20) / 100
   };
 
   $wheel.css({
     'transition-timing-function': 'cubic-bezier(0,' + object.x + ',' + object.y + ',1)',
-    'transition-duration': '2s',
+    'transition-duration': '4s',
     'transform': 'translate3d(-' + landingPosition + 'px, 0px, 0px)'
   });
 
@@ -94,11 +94,5 @@ function spinWheel(roll) {
 
     var resetTo = -(position * card + randomize);
     $wheel.css('transform', 'translate3d(' + resetTo + 'px, 0px, 0px)');
-  }, 2 * 1000);
+  }, 4 * 1000);
 }
-
-
-
-
-
-
